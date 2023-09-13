18541410946a7f18a697f124fe58fe6f8d4c3084
Mwasigwa2 <gathirimu.mwasigwa@student.moringaschool.com>
2023-09-13 11:01:01 +0300

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def get_title(self):
+        return self.title
+
+    def get_author(self):
+        return self.author
+
+    def get_pages(self):
+        return self.pages
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
